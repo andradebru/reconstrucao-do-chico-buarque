47418a908109cd2153c9088efa2c4bbb6fb9bc0c
andradebru <brunansandrade@gmail.com>
2023-02-06 23:14:39 -0300

    queria q tivesse funcionado mas inda n funcionou

--- frontend/components/GridOfGigs.vue
+++ frontend/components/GridOfGigs.vue
@@ -1,49 +1,21 @@
 <template>
-  <v-card class="mx-auto" max-width="500">
-    <v-icon>mdi-window-minimize</v-icon>
-
-    <v-icon>mdi-window-maximize</v-icon>
-
-    <v-icon>mdi-close</v-icon>
-
-    <v-spacer></v-spacer>
-
-    <v-container fluid>
-      <v-row dense>
-        <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
-          <v-card>
-            <v-img
-              :src="card.src"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="200px"
-            >
-              <v-card-title v-text="card.title"></v-card-title>
-            </v-img>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-card>
+  <v-container fluid blue>
+    <v-row dense>
+      <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
+        v-for of gigs
+        <GigCard />
+        <!-- <GigCard />
+        <GigCard /> -->
+        <!-- <GigCard />
+        <GigCard /> -->
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import GridOfGigs from "~/components/GridOfGigs.vue";
+import PickDate from "~/components/PickDate.vue";
 import GigCard from "~/components/GigCard.vue";
 
 export default {
