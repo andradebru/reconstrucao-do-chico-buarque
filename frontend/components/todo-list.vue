8d5bebe853ff9106cbcf646979cffebd8fe8e912
andradebru <brunansandrade@gmail.com>
2023-02-09 23:45:41 -0300

    v-for de versos e palavras, ta quase bora time

--- frontend/components/todo-list.vue
+++ frontend/components/todo-list.vue
@@ -1,10 +1,26 @@
 <template>
-  <v-card>
-    <v-progress-linear :indeterminate="true" v-if="loading"></v-progress-linear>
-        <v-card v-for="item in randomData" :key="item.palavra">
-          <p>{{item.palavra}} </p>
+  <v-container>
+    <v-card>
+      <v-progress-linear :indeterminate="true" v-if="loading"></v-progress-linear>
+      <v-card v-for="verso in versos" :key="verso.verso">
+            <p>{{verso.verso}} </p>
+        <!-- <v-simple-table dense>
+           <template 
+            v-for="item in randomData"
+            v-slot: [`item.${item.palavra}`]
+            >
+             <span :key="item.palavra">
+            <tbody> -->
+            
+          <!-- </tbody>
+           </template> -->
         </v-card>
-  </v-card>
+        palavras randomizadas
+          <v-card v-for="item in randomData" :key="item.palavra">
+            <p>{{item.palavra}} </p>
+          </v-card>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
@@ -13,12 +29,14 @@ export default {
   data () {
     return {
       items: [],
+      versos: [],
       randomData: []
     }
   },
   mounted () {
     AppApi.palavras_aleatoriaveis().then(response => {
       this.items = response.data.palavras
+      this.versos = response.data.versos
       for (let i = 0; i < 18; i++)
       this.randomData.push(this.randomItem(this.items))
       // this.randomData = this.randomItem(this.items)
