bc765c4d7e143f329c901c9640232bdb3eac00da
andradebru <brunansandrade@gmail.com>
2023-02-09 21:56:08 -0300

    lista e randomiza palavras da api, amem

--- frontend/components/todo-list.vue
+++ frontend/components/todo-list.vue
@@ -1,30 +1,10 @@
 <template>
   <v-card>
-    <!-- <v-toolbar color="cyan" dark> -->
-      <!-- <v-text-field class="ma-4" v-model="newtask" @keyup.enter="add()" label="Add todo and hit enter" :loading="adding"/> -->
-      <v-text-field class="ma-4" :loading="adding"/>
-    <!-- </v-toolbar> -->
     <v-progress-linear :indeterminate="true" v-if="loading"></v-progress-linear>
-    <!-- <v-list two-line> -->
-      <!-- <template v-for="palavra in palavras"> -->
-      <!-- <template v-for="item in items"> -->
-        <!-- <v-list-tile :key="palavra.palavra"> -->
-        <!-- <v-list-tile :key="item.title"> -->
-        <!-- <v-card v-for="item in items" :key="item.title"> -->
         <v-card v-for="item in items" :key="item.palavra">
-          <!-- <v-list-tile-content> -->
-            <!-- <v-list-tile-title > -->
-            <!-- <v-list-tile-title :class="{done: item.done}" > -->
-              <p>{{item.palavra}} </p>
-              <!-- {{item.description}} -->
-            <!-- </v-list-tile-title> -->
-          <!-- </v-list-tile-content> -->
-          <!-- <v-list-tile-action> -->
-            <!-- <v-switch v-model="item.done"/> -->
-          <!-- </v-list-tile-action> -->
+          <p>{{item.palavra}} </p>
         </v-card>
-      <!-- </template> -->
-    <!-- </v-list> -->
+          palavra randomizada: <b>{{randomData.palavra}}</b>
   </v-card>
 </template>
 
@@ -33,39 +13,22 @@ import AppApi from '~apijs'
 export default {
   data () {
     return {
-      // newtask: '',
-      // adding: false,
-      loading: false,
-      items: []
+      items: [],
+      randomData: []
     }
   },
   mounted () {
-    this.loading = true
     AppApi.palavras_aleatoriaveis().then(response => {
-    // AppApi.list_todos().then(response => {
-      const palavras = response.data.palavras
-      // const todos = response.data.todos
-      this.items = palavras
-      // this.items = todos
-      this.loading = false
+      this.items = response.data.palavras
+      this.randomData = this.randomItem(this.items)
     })
   },
-  // methods: {
-  //   add () {
-  //     this.adding = true
-  //     AppApi.add_todo(this.newtask).then(response => {
-  //       const todo = response.data
-  //       this.items.push(todo)
-  //       this.newtask = ''
-  //       this.adding = false
-  //     })
-  //   }
-  // }
-}
 
-// <style scoped>
-//   .done {
-//     text-decoration: line-through;
-//   }
-// </style>
+
+  methods: {
+    randomItem (items) {
+         return items[Math.floor(Math.random() * items.length)];
+     }
+  }
+}
 </script>
